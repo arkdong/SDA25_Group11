471b1f1219a12cd0c5561e654b78eecd91d3bb7f
Adam <ark.dong@outlook.com>
2025-11-27 23:18:54 +0100

    Add new helper to load Bitcoin Tweets with sentiment

--- src/read_data/helper.py
+++ src/read_data/helper.py
@@ -18,9 +18,32 @@ def load_16M_tweet(columns, csv_path):
         separator=";",
         schema=schema,
         has_header=True
-    ).select(columns)
+    )
 
-    return df
+    # If columns is empty, return all columns
+    if not columns:
+        return df
+
+    return df.select(columns)
+
+def load_tweet_with_sentiment(columns, csv_path):
+    schema = {
+        "Date": pl.Datetime, # or pl.Utf8 if raw text
+        "text": pl.Utf8,
+        "Sentiment": pl.Utf8
+    }
+
+    df = pl.read_csv(
+        csv_path,
+        separator=",",
+        schema=schema,
+        has_header=True
+    )
+
+    if not columns:
+        return df
+
+    return df.select(columns)
 
 if __name__ == "__main__":
     df = load_16M_tweet(["id", "timestamp", "text"], "data/tweets.csv")

4555cfe2deeca0c1d050da5690215b5fd2da8ea5
Petra Kampus <petra.kampus@student.uva.nl>
2025-12-03 18:15:19 +0100

    Hourly data for google searches, adding a function for said data to data.py and visualising the data.

--- src/helper/data.py
+++ src/helper/data.py
@@ -85,6 +85,87 @@ def load_data_sentiment(csv_path):
     )
     return df
 
+def load_btc(csv_path):
+    """
+    Load OHLCV dataset of bitcoin with Unix timestamps into a Polars DataFrame.
+
+    Expected columns:
+        Timestamp (Unix time)
+        Open
+        High
+        Low
+        Close
+        Volume
+
+    Parameters
+    ----------
+    csv_path : str
+        Path to the CSV file.
+
+    Returns
+    -------
+    pl.DataFrame
+        A Polars DataFrame with proper schema and timestamp parsed to Datetime.
+    """
+
+    schema = {
+        "timestamp": pl.Float64,   # Unix timestamp in seconds
+        "open": pl.Float64,
+        "high": pl.Float64,
+        "low": pl.Float64,
+        "close": pl.Float64,
+        "volume": pl.Float64,
+    }
+
+    df = pl.read_csv(
+        csv_path,
+        schema=schema,
+        has_header=True,
+        separator=","
+    )
+
+    # Convert Unix timestamp → Datetime
+    df = df.with_columns(
+        pl.from_epoch(pl.col("timestamp"), time_unit="s").alias("timestamp")
+    )
+
+    return df
+
+def load_btc_2018_2019(csv_path):
+    schema = {
+        "timestamp": pl.Datetime,   # Unix timestamp in seconds
+        "open": pl.Float64,
+        "high": pl.Float64,
+        "low": pl.Float64,
+        "close": pl.Float64,
+        "volume": pl.Float64,
+    }
+
+    df = pl.read_csv(
+        csv_path,
+        schema=schema,
+        has_header=True,
+        separator=","
+    )
+
+    return df
+
+def load_google_search(csv_path):
+    schema = {
+        "date": pl.Datetime,   # Unix timestamp in seconds
+        "Scale_['bitcoin']": pl.Float64
+    }
+
+    df = pl.read_csv(
+        csv_path,
+        schema=schema,
+        has_header=True,
+        separator=","
+    )
+
+    return df
+
+
 if __name__ == "__main__":
     df1 = load_btc_2018_2019("data/btc_2018_2019.csv")
     print(df1.head(2))
